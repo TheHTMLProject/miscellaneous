4fb4afcdbc6630e684e107920663b544b60b5806
The HTML Project <159310111+TheHTMLProject@users.noreply.github.com>
2025-11-04 11:58:56 -0800

    Delete VS Code/THP directory

--- VS Code/THP/Deprecated/global-message.js
+++
@@ -1,6 +0,0 @@
-// Global deprecated alert 
-  const alertKey = 'alertShown';
-  if (!localStorage.getItem(alertKey)) {
-    alert('This page is deprecated. It is old work that has either been replaced by something else or decided that it will not (for now) be a feature on my website. Please visit https://thehtmlproject.com for the main site. Thanks!');
-    localStorage.setItem(alertKey, 'true');
-  }
